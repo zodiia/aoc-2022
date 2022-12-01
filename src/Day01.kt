4a97d79b4664f4807338d73484a0f54cfde73d12
Manon Grivot <manongrivot42@gmail.com>
2022-12-01 17:47:12 +0100

    Finished Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
+package day01
+
+import readInput
+
 fun main() {
+    fun parseElves(input: List<String>): List<Int> {
+        val elves = ArrayList<Int>()
+        var currentElf = 0
+
+        input.forEach {
+            if (it.isBlank()) {
+                elves.add(currentElf)
+                currentElf = 0
+            } else {
+                currentElf += it.toInt()
+            }
+        }
+        elves.add(currentElf)
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = parseElves(input)
+
+        // Find the elf with the most calories
+        return elves.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val elves = parseElves(input)
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        // Find the top three elves
+        return elves.sortedDescending().take(3).sum()
+    }
 
     val input = readInput("Day01")
     println(part1(input))
